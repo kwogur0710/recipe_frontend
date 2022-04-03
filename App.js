b4572189243da022441a19222409fe0e3db8959b
Jaehyeok Kang <kwogur0710@naver.com>
2022-04-03 13:48:04 +0900

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>master 22/4/3</Text>
+            <Text>Jaehyeok test 22/4/3</Text>
             <StatusBar style="auto" />
         </View>
     );
