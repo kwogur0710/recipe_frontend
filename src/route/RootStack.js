ea3839d43befb5dd608a2ea52d2b83b44ba87a87
Jaehyeok Kang <kwogur0710@naver.com>
2022-05-26 00:49:41 +0900

    test

--- src/route/RootStack.js
+++ src/route/RootStack.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import MainScreen from '../screen/MainScreen';
 import RecipeListScreen from '../screen/RecipeListScreen';
@@ -10,21 +11,56 @@ import RecipeBoardScreen from '../screen/RecipeboardScreen';
 import RecipeAddScreen from '../screen/RecipeAddScreen';
 import SettingScreen from '../screen/SettingScreen';
 import SearchScreen from '../screen/SearchScreen';
+import { MaterialCommunityIcons, Feather } from '@expo/vector-icons';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabIcon = ({ name, size, color }) => {
+    return <Feather name={name} size={size} color={color} />;
+};
 
 const RootStack = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Main" screenOptions={{ headerMode: false }}>
-                <Stack.Screen name="MainScreen" component={MainScreen} />
-                <Stack.Screen name="RecipeListScreen" component={RecipeListScreen} />
-                <Stack.Screen name="RecipeIngredientsScreen" component={RecipeIngredientsScreen} />
-                <Stack.Screen name="RecipeBoardScreen" component={RecipeBoardScreen} />
-                <Stack.Screen name="RecipeAddScreen" component={RecipeAddScreen} />
-                <Stack.Screen name="SettingScreen" component={SettingScreen} />
-                <Stack.Screen name="SearchScreen" component={SearchScreen} />
-            </Stack.Navigator>
+            <Tab.Navigator
+                initialRouteName="MainScreen"
+                shifting={true}
+                activeColor="#f0edf6"
+                inactiveColor="#3e2465"
+                barStyle={{ backgroundColor: '#694fad' }}
+                labeled={true}
+                backBehavior={"none"}
+            >
+                <Tab.Screen
+                    name="MainScreen"
+                    component={MainScreen}
+                    options={{
+                        tabBarIcon: (props) => TabIcon({ ...props, name: 'home' }),
+                    }}
+                />
+                <Tab.Screen
+                    name="SearchScreen"
+                    component={SearchScreen}
+                    options={{
+                        tabBarIcon: (props) => TabIcon({ ...props, name: 'search' }),
+                    }}
+                />
+                <Tab.Screen
+                    name="RecipeAddScreen"
+                    component={RecipeAddScreen}
+                    options={{
+                        tabBarIcon: (props) => TabIcon({ ...props, name: 'plus' }),
+                    }}
+                />
+                <Tab.Screen
+                    name="SettingScreen"
+                    component={SettingScreen}
+                    options={{
+                        tabBarIcon: (props) => TabIcon({ ...props, name: 'settings' }),
+                    }}
+                />
+            </Tab.Navigator>
         </NavigationContainer>
     );
 };
